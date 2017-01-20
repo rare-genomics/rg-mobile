cbe4d32c0812fef688a3ddc65f7e6c70835b983b
James Lang <thejameslang@Jamess-MacBook-Pro.local>
2017-01-19 18:13:04 -0600

    Alert for Facebook login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { AlertController } from 'ionic-angular';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) { }
+
+  doAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Sorry!',
+      message: 'That feature has not been implemented yet!',
+      buttons: ['Ok']
+    });
+    alert.present()
   }
 
 }
