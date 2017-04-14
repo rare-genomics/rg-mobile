5d3ab5682fda59caf01d3238013e04ce72faa724
Thiago B. Maia <thiago.maia@raregenomics.org>
2017-04-14 03:14:17 -0300

    Fixing conflits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen, CallNumber } from 'ionic-native';
+// import { StatusBar, Splashscreen, CallNumber } from 'ionic-native';
+// import { Splashscreen, CallNumber } from 'ionic-native';
+import { StatusBar } from 'ionic-native';
+import { Splashscreen, CallNumber } from 'ionic-native';
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HomePage } from '../pages/home/home';
 import { MedhomePage } from '../pages/medhome/medhome';
 import { CaregiverPage } from '../pages/caregiver/caregiver';
-import { PreferencesPage } from '../pages/preferences/preferences';
+// import { PreferencesPage } from '../pages/preferences/preferences';
 import { PatientProfilePage } from '../pages/patient-profile/patient-profile';
 
 @Component({
@@ -18,26 +21,26 @@ export class MyApp {
 
   // pages: Array<{title: string, component: any}>;
 
-  // constructor(platform: Platform) {
-  //   platform.ready().then(() => {
-  //     StatusBar.styleDefault();
-  //     Splashscreen.hide();
-  //   });
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+    });
 
-  //   this.pages = [
-  //     { title : 'My Preferences', component: PreferencesPage },
-  //     { title : 'Get a Diagnosis', component: CaregiverPage },
-  //     { title : 'What is Rare Genomics', component: CaregiverPage },
-  //     { title : '-- Our Mission', component: CaregiverPage },
-  //     { title : '-- Who We Are', component: CaregiverPage },
-  //     { title : '-- Rare Disease Facts', component: CaregiverPage },
-  //     { title : '-- Contact Us', component: CaregiverPage },
-  //     { title : 'Patient Resources', component: CaregiverPage },
-  //     { title : '-- RareReach', component: CaregiverPage },
-  //     { title : '-- FAQ', component: CaregiverPage },
-  //     { title : 'Rare Genomics Institute News', component: CaregiverPage }
-  //   ];
-  // }
+    //   this.pages = [
+    //     { title : 'My Preferences', component: PreferencesPage },
+    //     { title : 'Get a Diagnosis', component: CaregiverPage },
+    //     { title : 'What is Rare Genomics', component: CaregiverPage },
+    //     { title : '-- Our Mission', component: CaregiverPage },
+    //     { title : '-- Who We Are', component: CaregiverPage },
+    //     { title : '-- Rare Disease Facts', component: CaregiverPage },
+    //     { title : '-- Contact Us', component: CaregiverPage },
+    //     { title : 'Patient Resources', component: CaregiverPage },
+    //     { title : '-- RareReach', component: CaregiverPage },
+    //     { title : '-- FAQ', component: CaregiverPage },
+    //     { title : 'Rare Genomics Institute News', component: CaregiverPage }
+    //   ];
+  }
 
   gotoMedicationHome() {
     this.nav.push(MedhomePage);
