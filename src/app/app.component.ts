fbaba0e6e420976b3305236b0ff3ac0a8b4fed39
Thejaswini Sunil <Thejaswini Sunil>
2017-04-20 15:52:18 -0700

    Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class MyApp {
   @ViewChild('mycontent') nav: NavController
   rootPage = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
@@ -25,19 +25,19 @@ export class MyApp {
       Splashscreen.hide();
     });
 
-      this.pages = [
-        { title : 'My Preferences', component: PreferencesPage },
-        { title : 'Get a Diagnosis', component: CaregiverPage },
-        { title : 'What is Rare Genomics', component: CaregiverPage },
-        { title : '-- Our Mission', component: CaregiverPage },
-        { title : '-- Who We Are', component: CaregiverPage },
-        { title : '-- Rare Disease Facts', component: CaregiverPage },
-        { title : '-- Contact Us', component: CaregiverPage },
-        { title : 'Patient Resources', component: CaregiverPage },
-        { title : '-- RareReach', component: CaregiverPage },
-        { title : '-- FAQ', component: CaregiverPage },
-        { title : 'Rare Genomics Institute News', component: CaregiverPage }
-      ];
+    this.pages = [
+      { title: 'My Preferences', component: PreferencesPage },
+      { title: 'Get a Diagnosis', component: CaregiverPage },
+      { title: 'What is Rare Genomics', component: CaregiverPage },
+      { title: '-- Our Mission', component: CaregiverPage },
+      { title: '-- Who We Are', component: CaregiverPage },
+      { title: '-- Rare Disease Facts', component: CaregiverPage },
+      { title: '-- Contact Us', component: CaregiverPage },
+      { title: 'Patient Resources', component: CaregiverPage },
+      { title: '-- RareReach', component: CaregiverPage },
+      { title: '-- FAQ', component: CaregiverPage },
+      { title: 'Rare Genomics Institute News', component: CaregiverPage }
+    ];
   }
 
   gotoMedicationHome() {
