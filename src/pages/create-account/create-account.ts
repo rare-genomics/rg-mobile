3422124c983f8723c68773d3c759cfc56554e1b5
Thiago B. Maia <thiago.maia@raregenomics.org>
2017-06-18 07:42:32 -0300

    Bug create account

--- src/pages/create-account/create-account.ts
+++ src/pages/create-account/create-account.ts
@@ -11,20 +11,21 @@ import { AlertController } from 'ionic-angular';
 })
 export class CreateAccountPage {
   localdata = {};
-  exists = false;
+  // exists = false;
   constructor(public navCtrl: NavController, private db: InitDatabase, public alertCtrl: AlertController) {
     this.loadData();
   }
 
   loadData() {
-    let bridge = { 'localdata': this.localdata, 'exists': this.exists };
+    // let bridge = { 'localdata': this.localdata, 'exists': this.exists };
+    let bridge = { 'localdata': this.localdata};
     this.db._db.transaction(function (tx) {
       tx.executeSql('SELECT firstname, lastname FROM profile WHERE id=1', [], function (tx, res) {
         var len = res.rows.length;
         for (var i = 0; i < len; i++) {
           bridge.localdata['firstname'] = res.rows.item(i).firstname;
           bridge.localdata['lastname'] = res.rows.item(i).lastname;
-          bridge.exists = true;
+          // bridge.exists = true;
         }
       }, function (e) {
       });
@@ -44,31 +45,37 @@ export class CreateAccountPage {
   }
 
   saveData() {
+    console.log("Saving data 1");
     if (this.replaceUndefined() == 1) {
+      console.log("Saving data 2");
       return;
     }
+    console.log("Saving data 3");
     let bridge = this.localdata;
-    if (this.exists) {
-      this.db._db.transaction(function (tx) {
-        tx.executeSql('UPDATE profile SET firstname = ?, lastname = ? WHERE id = 1', [
-          bridge['firstname'],
-          bridge['lastname']
-        ], function (tx, res) {
-        }, function (e) {
-          console.log(e.message + " Error updating the database " + e);
-        });
+    // if (this.exists) {
+    console.log("Saving data 4");
+    console.log("Updating name and surname");
+    this.db._db.transaction(function (tx) {
+      tx.executeSql('UPDATE profile SET firstname = ?, lastname = ? WHERE id = 1', [
+        bridge['firstname'],
+        bridge['lastname']
+      ], function (tx, res) {
+      }, function (e) {
+        console.log(e.message + " Error updating the database " + e);
       });
-    } else {
-      this.db._db.transaction(function (tx) {
-        tx.executeSql('INSERT INTO profile (id, firstname, lastname) VALUES (1, ?, ?)', [
-          bridge['firstname'],
-          bridge['lastname']
-        ], function (tx, res) {
-        }, function (e) {
-          console.log(e.message + " Error updating the database " + e);
-        });
-      });
-    }
+    });
+    // } 
+    // else {
+    //   this.db._db.transaction(function (tx) {
+    //     tx.executeSql('INSERT INTO profile (id, firstname, lastname) VALUES (1, ?, ?)', [
+    //       bridge['firstname'],
+    //       bridge['lastname']
+    //     ], function (tx, res) {
+    //     }, function (e) {
+    //       console.log(e.message + " Error updating the database " + e);
+    //     });
+    //   });
+    // }
     this.navCtrl.push(EnterBirthdayPage);
   }
 
