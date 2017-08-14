0a05e346ed7731f362a73c985f726adf8582adea
Thiago <a00s@a00s.com>
2017-08-14 12:21:22 -0300

    Some fix

--- src/pages/email-address/email-address.ts
+++ src/pages/email-address/email-address.ts
@@ -49,7 +49,7 @@ export class EmailAddressPage {
     let body = this.jsonToURLEncoded({
       email: this.localdata['email']
     });
-    var link = 'http://127.0.0.1:3000/api/checkemail';
+    var link = 'https://mobile.raregenomics.org/api/checkemail';
     let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' });
     let options = new RequestOptions({
       headers: headers
